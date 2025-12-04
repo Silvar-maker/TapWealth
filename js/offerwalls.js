94d182700083138c6c17fe08e47b2202e2bc29cb
Silvar <wealthysilvar@gmail.com>
2025-12-04 17:10:40 +0100

    Add loadOfferwalls function to load offerwalls

---
+++ js/offerwalls.js
@@ -0,0 +1,7 @@
+function loadOfferwalls() {
+  document.getElementById("offerwalls").innerHTML = `
+    <iframe src="https://wall.adgaterewards.com/YOUR-ID" class="wall"></iframe>
+    <iframe src="https://www.offertoro.com/ifr/show/YOUR-ID" class="wall"></iframe>
+    <iframe src="https://cpalead.com/offerwall/YOUR-ID" class="wall"></iframe>
+  `;
+}
