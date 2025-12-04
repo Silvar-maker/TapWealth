bb7fdee5b78febc7699ee18c1d59606a4de03f55
Silvar <wealthysilvar@gmail.com>
2025-12-04 17:10:04 +0100

    Delete offerwalls.js

--- offerwalls.js
+++
@@ -1,7 +0,0 @@
-function loadOfferwalls() {
-  document.getElementById("offerwalls").innerHTML = `
-    <iframe src="https://wall.adgaterewards.com/YOUR-ID" class="wall"></iframe>
-    <iframe src="https://www.offertoro.com/ifr/show/YOUR-ID" class="wall"></iframe>
-    <iframe src="https://cpalead.com/offerwall/YOUR-ID" class="wall"></iframe>
-  `;
-}
